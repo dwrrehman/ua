0f4b42fb53e3b23818ec42ca048156fcd8777a8d
dwrrehman <dwrrehman@gmail.com>
2019-12-30 22:34:11 -0800

    change testing code slightly.

--- test/ca/main.c
+++ test/ca/main.c
@@ -1,40 +1,46 @@
-// current state of the ua theory.
+/// current state of the ua theory.
+
+// extraneous:
 #include <unistd.h> // using:  usleep()
 #include <stdio.h>  // using:  printf()
-#include <stdlib.h> // using: atoll(), atoi()
+#include <stdlib.h> // using: atoll()
 
 // actually needed:
-#include <string.h>
-#include <math.h>
+#include <string.h> // using memset(), memcpy().
+#include <math.h> // using powl()
 
 int main(int argc, const char** argv) {
-    if (argc <= 5) { printf("usage: \n\t./ca m n l d t\n\n"); return 1; }
-    const unsigned long long m = atoll(argv[1]), n = atoll(argv[2]), s = atoll(argv[3]), S = powl(s, n);
-    const unsigned int delay = atoi(argv[4]), lifetime = atoi(argv[5]);
+    if (argc <= 5) { printf("usage: \n\t./ca m n s t d\n\n"); return 1; }
+    const unsigned long long
+        m = atoll(argv[1]),
+        n = atoll(argv[2]),
+        s = atoll(argv[3]), S = powl(s, n),
+        t = atoll(argv[4]),
+        d = atoll(argv[5]);
+    
     unsigned long long g[S], h[S];
     memset(g, 0, sizeof g);
     g[0] = 1;
-    for (unsigned long long i = 0; i < lifetime; i++) {
+    
+    for (unsigned long long i = 0; i < t; i++) {
         if (i % 200 == 0 && i) printf("\e[1;1H\e[2J");
         memcpy(h, g, sizeof h);
         for (unsigned long long j = 0; j < S; j++) {
-            
+                                    
             const unsigned long long l = h[(j + s + 1) % s];
             const unsigned long long r = h[(j + s - 1) % s];
             const unsigned long long c = h[j];
             
-            const unsigned long long e = c * (l * (l + 2) + 1);
+            const unsigned long long e = (     c * (l + 1)         ) % m;
+            const unsigned long long u = (     m - e + m - 1       ) % m;
             
-            const unsigned long long d = c * (l + 1);
+            g[j] -= r * u;
+            g[j] %= m;
             
-            const unsigned long long u = r * (e + d + 1);
-            
-            g[j] = (c + u) % m;
-            
-            printf("\033[38;5;%dm██\033[0m", (int)(24.0 * (double) h[j] / m) + 232);
+            printf("\033[38;5;%um██\033[0m", (unsigned)((double) h[j] / m * 24) + 232);
         }
         printf("\n");
-        usleep(delay);
+        usleep((unsigned) d);
     }
     return 0;
 }
@@ -50,8 +56,7 @@ int main(int argc, const char** argv) {
 
 
 
-
-
+/// STORAGE FOR DEAD CODE:
 
 
 //for (int f = 1; f < L; f *= l) {
@@ -59,4 +64,8 @@ int main(int argc, const char** argv) {
 //    g[j] %= m;
 //}
 
-//            const long long left = h[(j + l - 1) % l];
+
+//
+//const unsigned long long e = c * (l * l + l + 1);
+//
+//const unsigned long long u = r * (e * e + e + 1);
