c6bacac3bb04e8167bc950421c0fee95040509c0
dwrrehman <dwrrehman@gmail.com>
2020-04-10 12:26:26 -0700

    update the repo to the current state of the project.

--- source/test/ca/main.c
+++ source/test/ca/main.c
@@ -9,7 +9,7 @@
 typedef unsigned long long nat;
 
 int main(int argc, const char** argv) {
-    if (argc <= 6) { printf("11: usage: \n\t./ca m n s t d nd\n\n"); return 1; }
+    if (argc <= 6) { printf("13: usage: \n\t./ca m n s t d nd\n\n"); return 1; }
     const nat
         m = atoll(argv[1]),
         n = atoll(argv[2]),
@@ -40,21 +40,71 @@ int main(int argc, const char** argv) {
             
             const nat r = pos[0];
             const nat l = neg[0];
-            
-            
-            
+        
             const nat d = neg[1];
             const nat u = pos[1];
             
     
-            const nat _22 = (
-                             
-                                u  *  ( c * (l + 1) * (d + 1) + 1 )
-                             
-                             ) % m;
-            const nat r110 = (r * (c * (l + 1) + 1)) % m;
+            const nat f =
             
-            g[_j] = (g[_j] + r110 + _22) % m;
+//            l+
+//            u+
+//            l*c+
+//            l*u+
+//            c*u+
+//            d*u+
+//            l*c*u+
+//            c*d*u+
+//            l*c*d*u+
+            
+            //======================
+            
+//            l+
+//            u+
+//            l*r+
+//            l*c+
+//            l*u+
+//            u*c+
+//            l*c*r+
+//            l*u*r+
+//            l*c*u+
+            
+            
+            // 1d term
+            l+
+            l*r+
+            l*c+
+            l*c*r+
+            
+            // 2d term
+            u*c*d+
+            u*l*d+
+            u*l*r*d+
+            u*l*c*d+
+            u*l*c*r*d+
+            u*c+
+            u*l+
+            u*l*r+
+            u*l*c+
+            u*l*c*r+
+            u*d+
+            u+
+            
+            
+//            u+
+////            u*d+
+//            u*l*d+
+//            u*l*r*d+
+//            u*l*c*r*d+
+//            u*l+
+//            u*l*r+
+//            u*l*c+
+//            u*l*c*r+
+                        
+            // - end of added -
+            0;
+                        
+            g[_j] = (c + f) % m;
                                     
             if (_j % s == 0 && nd) printf("\n");
             printf("\033[38;5;%um██\033[0m", (unsigned)((double) h[_j] / m * 24) + 232);
