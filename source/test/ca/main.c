dea27a39d2cdfc6bbb810957547cca51bc67af7f
dwrrehman <dwrrehman@gmail.com>
2020-04-27 14:19:47 -0700

    add the new testing code, we are playing around with different n and m loops, because now we know the bugbudget pinrciple.

--- source/test/ca/main.c
+++ source/test/ca/main.c
@@ -8,112 +8,54 @@
 #include <math.h>   // using powl()
 typedef unsigned long long nat;
 
+#define N1 for (nat i = 0; i < n; i++)
+#define M1 for (nat j = 1; j < m; j++)
+
+#define N2 for (nat k = 0; k < n; k++)
+#define M2 for (nat l = 1; l < m; l++)
+
+#define N3 for (nat _i = 0; _i < n; _i++)
+#define M3 for (nat _j = 1; _j < m; _j++)
+
+#define P for (nat _p = 0; _p < p[i]; _p++, f[s] = (f[s] + 1) % m)
+#define O for (nat o = f[s], _o = 0; _o < o; _o++, f[s] = (f[s] + 1) % m)
+#define Q for (nat _q = 0; _q < q[k]; _q++, f[s] = (f[s] + 1) % m)
+
 int main(int argc, const char** argv) {
-    if (argc <= 6) { printf("13: usage: \n\t./ca m n s t d nd\n\n"); return 1; }
+    if (argc <= 6) { printf("17: usage: \n\t./ca m n s t d nd\n\n"); return 1; }
     const nat
         m = atoll(argv[1]),
         n = atoll(argv[2]),
-        s = atoll(argv[3]), S = powl(s, n),
-        t = atoll(argv[4]),
-        d = atoll(argv[5]),
+        S = atoll(argv[3]), S_n = powl(S, n),
+        T = atoll(argv[4]),
+        delay = atoll(argv[5]),
         nd = atoll(argv[6]);
     
-    nat g[S], h[S], pos[n], neg[n];
+    nat f[S_n], g[S_n], p[n], q[n];
     
-    memset(g, 0, sizeof g);
-    if (n == 2) g[S / 2 + s / 2] = 1;
-    else ++*g;
+    memset(f, 0, sizeof f);
+    if (n == 2) f[S_n / 2 + S / 2] = 1;
+    else ++*f;
     
-    for (nat _i = 0; _i < t; _i++) {
-        if ((_i % 200 == 0 && _i) || nd) printf("\e[1;1H\e[2J");
-        memcpy(h, g, sizeof h);
-        for (nat _j = 0; _j < S; _j++) {
+    for (nat t = 0; t < T; t++) {
+        if (nd) printf("\e[1;1H\e[2J");
+        memcpy(g, f, sizeof g);
+        for (nat s = 0; s < S_n; s++) {
             
             nat y = 0;
-            for (nat f = 1; f < S; f *= s) {
-                pos[y] = h[_j + f * ((_j / f + 1) % s - _j / f % s)];
-                neg[y] = h[_j + f * ((_j / f + s - 1) % s - _j / f % s)];
-                y++;
+            for (nat k = 1; k < S_n; k *= S) {
+                p[y] = g[s + k * ((s / k + 1) % S - s / k % S)];
+                q[y++] = g[s + k * ((s / k + S - 1) % S - s / k % S)];
             }
-    
-            const nat c = h[_j];
             
-            const nat r = pos[0];
-            const nat l = neg[0];
-        
-            const nat d = neg[1];
-            const nat u = pos[1];
+            N1 M1 P M3 O N2 M2 Q;
             
-    
-            const nat f =
+            if (s % S == 0 && nd) printf("\n");
+            printf("\033[38;5;%um██\033[0m", (unsigned)((double) g[s] / m * 24) + 232);
             
-//            l+
-//            u+
-//            l*c+
-//            l*u+
-//            c*u+
-//            d*u+
-//            l*c*u+
-//            c*d*u+
-//            l*c*d*u+
-            
-            //======================
-            
-//            l+
-//            u+
-//            l*r+
-//            l*c+
-//            l*u+
-//            u*c+
-//            l*c*r+
-//            l*u*r+
-//            l*c*u+
-            
-            
-            // 1d term
-            l+
-            l*r+
-            l*c+
-            l*c*r+
-            
-            // 2d term
-            u*c*d+
-            u*l*d+
-            u*l*r*d+
-            u*l*c*d+
-            u*l*c*r*d+
-            u*c+
-            u*l+
-            u*l*r+
-            u*l*c+
-            u*l*c*r+
-            u*d+
-            u+
-            
-            
-//            u+
-////            u*d+
-//            u*l*d+
-//            u*l*r*d+
-//            u*l*c*r*d+
-//            u*l+
-//            u*l*r+
-//            u*l*c+
-//            u*l*c*r+
-                        
-            // - end of added -
-            0;
-                        
-            g[_j] = (c + f) % m;
-                                    
-            if (_j % s == 0 && nd) printf("\n");
-            printf("\033[38;5;%um██\033[0m", (unsigned)((double) h[_j] / m * 24) + 232);
-            fflush(stdout);
         }
         printf("\n");
-        usleep((unsigned) d);
-//        getc(stdin);
+        fflush(stdout);
+        usleep((unsigned) delay);
     }
 }
-
-//            g[_j] = (h[_j] + pos[0] * (h[_j] * (neg[0] + 1) + 1 )) % m;   /// rule 110.     (2,1) case.
