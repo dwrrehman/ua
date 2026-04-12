c397aac627be9c5af4c9ab2a2114027e232f7c39
Daniel Rehman <dwrrehman@gmail.com>
2026-04-11 19:30:45 -0700

    make some huge changes to how the search utility outputs its progress, we are now instead using a file to store the programs output, and we output to it when receiving a hangup signal lol. works well i think. time to start up an actaul 2 space call on the mac minis, using our ssh scripts that we wrote too.

---
+++ alternative_to_networked_utility/commands_executed_on_mini.sh
@@ -0,0 +1,58 @@
+#!/bin/zsh
+# useful script executing commands on a mac mini. 
+# written on 1202604046.204915
+
+cd root/projects/ua
+git pull
+cd ~
+exit
+
+
+
+# cd calls/17/
+# ./build release
+# screen -d -m session_name "./run $1"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ alternative_to_networked_utility/ssh_transfer_script.sh
@@ -0,0 +1,122 @@
+#!/bin/zsh
+# useful ssh script to execute commands on all mac minis. 
+# written on 1202604046.203804
+
+scp commands_executed_on_mini.sh dwrr-mini.local:/Users/dwrr/root/
+scp commands_executed_on_mini.sh dwrr-mini1.local:/Users/dwrr/root/
+scp commands_executed_on_mini.sh dwrr-mini2.local:/Users/dwrr/root/
+scp commands_executed_on_mini.sh dwrr-mini3.local:/Users/dwrr/root/
+scp commands_executed_on_mini.sh dwrr-mini4.local:/Users/dwrr/root/
+scp commands_executed_on_mini.sh dwrr-mini5.local:/Users/dwrr/root/
+scp commands_executed_on_mini.sh dwrr-mini6.local:/Users/dwrr/root/
+scp commands_executed_on_mini.sh dwrr-mini7.local:/Users/dwrr/root/
+scp commands_executed_on_mini.sh dwrr-mini8.local:/Users/dwrr/root/
+scp commands_executed_on_mini.sh dwrr-mini9.local:/Users/dwrr/root/
+
+ssh dwrr-mini.local "./root/commands_executed_on_mini.sh 0"
+ssh dwrr-mini1.local "./root/commands_executed_on_mini.sh 1"
+ssh dwrr-mini2.local "./root/commands_executed_on_mini.sh 2"
+ssh dwrr-mini3.local "./root/commands_executed_on_mini.sh 3"
+ssh dwrr-mini4.local "./root/commands_executed_on_mini.sh 4"
+ssh dwrr-mini5.local "./root/commands_executed_on_mini.sh 5"
+ssh dwrr-mini6.local "./root/commands_executed_on_mini.sh 6"
+ssh dwrr-mini7.local "./root/commands_executed_on_mini.sh 7"
+ssh dwrr-mini8.local "./root/commands_executed_on_mini.sh 8"
+ssh dwrr-mini9.local "./root/commands_executed_on_mini.sh 9"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#ssh-keygen
+#
+#ssh-copy-id dwrr-mini.local
+#ssh-copy-id dwrr-mini1.local
+#ssh-copy-id dwrr-mini2.local
+#ssh-copy-id dwrr-mini3.local
+#ssh-copy-id dwrr-mini4.local
+#ssh-copy-id dwrr-mini5.local
+#ssh-copy-id dwrr-mini6.local
+#ssh-copy-id dwrr-mini7.local
+#ssh-copy-id dwrr-mini8.local
+#ssh-copy-id dwrr-mini9.local
+#
+#
+
+
+#ssh dwrr-mini.local
+#cd root/projects/ua
+#git pull
+#git status 
+#cd ~
+#exit
+
+#ssh dwrr-mini1.local
+#cd root/projects/ua
+#git pull
+#git status 
+#cd ~
+#exit
+
+# ...
+
+#ssh dwrr-mini9.local
+#cd root/projects/ua
+#git pull
+#git status 
+#cd ~
+#exit
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
